87f66b0b0c9faa886066ea5111151dfa0486ccea
Nischal Samji <nischalsamji@gmail.com>
2016-05-01 17:44:52 -0700

    Fix Users List Bug

--- helpers/common.js
+++ helpers/common.js
@@ -1,10 +1,13 @@
 var loggedUsers = [];
 
-
 exports.adduser = function(userid){
-    if(!(userid in loggedUsers))
+    var flag = 0;
+    for(i =0; i< loggedUsers.length; i++)
     {
-        console.log("User already logged in");
+     if(loggedUsers[i]== userid)
+        flag = 1
+    }
+    if(flag  == 0){
         loggedUsers.push(userid);
     }
 }
