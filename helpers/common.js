458229d032c639470acba8b1f1361a0cfb92e343
Nischal Samji <nischalsamji@gmail.com>
2016-05-01 17:24:28 -0700

    Fix Multiple users

--- helpers/common.js
+++ helpers/common.js
@@ -2,7 +2,6 @@ var loggedUsers = [];
 
 
 exports.adduser = function(userid){
-    console.log('user added to list');
     if(!(userid in loggedUsers))
     {
         console.log("User already logged in");
